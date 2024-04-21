d3bf4c51123b9636bdfb87c306f0c94c190341f7
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-04-21 13:19:21 -0700

    Set up basic layout
    Add header, main, and set up the library grid.

---
+++ script.js
@@ -0,0 +1,12 @@
+const library = [];
+
+function Book(title, author, numPages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary() {
+    
+}
