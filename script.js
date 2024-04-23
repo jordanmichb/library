cba1a21179dfccc865a5a1975a73855dca5c7a00
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-04-23 16:20:15 -0700

    Write JavaScript to display books from array
    Created three book objects as examples and wrote a function to create
    a new DOM element for each book and display it in the lbrary.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const info = document.querySelector(".info");
 const menuBtn = document.querySelector(".menu-btn");
+const library = document.querySelector(".library");
 
 menuBtn.addEventListener('click', () => {
     info.classList.toggle('collapsed');
@@ -14,7 +15,51 @@ function Book(title, author, pageCount, hasRead) {
     this.hasRead = hasRead;
 }
 
-function addBookToLibrary() {
+const book1 = new Book('Book1', 'Jordan Ballard', 900, true);
+const book2 = new Book('Book2', 'Trenten Nash', 407, false);
+const book3 = new Book('Book3', 'Trier Perry', 564, true);
+booksList.push(book1)
+booksList.push(book2)
+booksList.push(book3)
 
+function addBookToLibrary(book) {
+    booksList.push(book);
 }
+
+function displayBooks() {
+    for (const book of booksList) {
+        const newBook = document.createElement("div");
+        newBook.classList.add('book', 'card');
+
+        const button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.classList.add('delete-btn');
+
+        const title = document.createElement('p');
+        title.classList.add('book-title');
+        title.textContent = book.title;
+
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.textContent = book.author;
+
+        const pageCount = document.createElement('p');
+        pageCount.classList.add('page-count');
+        pageCount.textContent = book.pageCount;
+
+        const hasRead = document.createElement('p');
+        hasRead.classList.add('has-read');
+        hasRead.textContent = book.hasRead ? 'Completed' : 'Not Finished';
+
+        newBook.appendChild(button);
+        newBook.appendChild(title);
+        newBook.appendChild(author);
+        newBook.appendChild(pageCount);
+        newBook.appendChild(hasRead);
+
+        library.appendChild(newBook);
+    }
+}
+
+displayBooks();
 
